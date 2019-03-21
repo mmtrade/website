3b8320683ea1990942aff19e1946c5b8eed44891
alxshelepenok <alxshelepenok@gmail.com>
2019-03-21 19:26:24 +0300

    chore(tests): add get-contact-href and get-icon

---
+++ src/utils/get-contact-href.test.js
@@ -0,0 +1,8 @@
+import getContactHref from './get-contact-href';
+
+test('getContactHref', () => {
+  expect(getContactHref('github', '#')).toBe('https://github.com/#'),
+  expect(getContactHref('discord', '#')).toBe('#'),
+  expect(getContactHref('gmail', '#')).toBe('mailto:#'),
+  expect(getContactHref('telegram', '#')).toBe('telegram:#')
+});
---
+++ src/utils/get-icon.test.js
@@ -0,0 +1,9 @@
+import getIcon from './get-icon';
+import { ICONS } from '../constants';
+
+test('getIcon', () => {
+  expect(getIcon('github')).toBe(ICONS.GITHUB),
+  expect(getIcon('discord')).toBe(ICONS.DISCORD),
+  expect(getIcon('gmail')).toBe(ICONS.GMAIL),
+  expect(getIcon('telegram')).toBe(ICONS.TELEGRAM)
+});

e042b20753c7b3f82b135e5b9aa03be75a38364b
alxshelepenok <alxshelepenok@gmail.com>
2019-03-21 19:11:23 +0300

    chore: remove unused icons

--- src/utils/get-contact-href.js
+++ src/utils/get-contact-href.js
@@ -2,20 +2,11 @@ const getContactHref = (name, contact) => {
   let href;
 
   switch (name) {
-    case 'twitter':
-      href = `https://www.twitter.com/${contact}`;
-      break;
     case 'github':
       href = `https://github.com/${contact}`;
       break;
-    case 'vkontakte':
-      href = `https://vk.com/${contact}`;
-      break;
     case 'telegram':
       href = `telegram:${contact}`;
-      break;
-    case 'email':
-      href = `mailto:${contact}`;
       break;
     case 'gmail':
       href = `mailto:${contact}`;
--- src/utils/get-icon.js
+++ src/utils/get-icon.js
@@ -4,9 +4,6 @@ const getIcon = (name) => {
   let icon;
 
   switch (name) {
-    case 'twitter':
-      icon = ICONS.TWITTER;
-      break;
     case 'github':
       icon = ICONS.GITHUB;
       break;
@@ -16,17 +13,8 @@ const getIcon = (name) => {
     case 'gmail':
       icon = ICONS.GMAIL;
       break;
-    case 'vkontakte':
-      icon = ICONS.VKONTAKTE;
-      break;
     case 'telegram':
       icon = ICONS.TELEGRAM;
-      break;
-    case 'email':
-      icon = ICONS.EMAIL;
-      break;
-    case 'rss':
-      icon = ICONS.RSS;
       break;
     default:
       icon = {};
